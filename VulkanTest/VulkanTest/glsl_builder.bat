0685b491e3603e1d44063e6c0f267d98346959c9
Zenny Chen <zenny_chen@sina.com>
2022-06-28 04:45:17 +0800

    Add files via upload

---
+++ VulkanTest/VulkanTest/glsl_builder.bat
@@ -0,0 +1,2 @@
+%VK_SDK_PATH%/Bin/glslc.exe  -fshader-stage=compute  -o simpleKernel.spv  simpleKernel.glsl
+%VK_SDK_PATH%/Bin/glslc.exe  -fshader-stage=compute  -o advancedKernel.spv  advancedKernel.glsl
