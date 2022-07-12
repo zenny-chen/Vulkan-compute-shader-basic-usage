1046063b69f3516d60ae4ca3eadd5d6bed1444ee
Zenny Chen <zenny_chen@sina.com>
2022-07-13 06:01:18 +0800

    Delete VulkanTest directory

--- VulkanTest/VulkanTest/glsl_builder.bat
+++
@@ -1,2 +0,0 @@
-%VK_SDK_PATH%/Bin/glslc.exe  -fshader-stage=compute  -o simpleKernel.spv  simpleKernel.glsl
-%VK_SDK_PATH%/Bin/glslc.exe  -fshader-stage=compute  -o advancedKernel.spv  advancedKernel.glsl
